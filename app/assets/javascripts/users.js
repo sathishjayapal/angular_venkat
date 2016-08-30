ed9d0b3006833cc2d8c92898395e8c1a137f5832
Sathish Jayapal <sjayapal@skminfotech.com>
2016-08-30 06:10:13 -0500

    Using controllers instruction

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,11 +1,30 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-var MyController= function() {
-    console.log("AM getting here");
-    var controller=this;
+var MyController = function () {
+    var controller = this;
+
     var greet = function () {
         return "howdy";
     }
-    controller.greet=greet;
+
+    var getFullName = function () {
+        console.log("Coming here");
+        if (controller.firstName == '')
+            controller.fullName = '';
+        if (controller.lastName == '')
+            controller.fullName = controller.firstName + '';
+        else
+            controller.fullName=controller.firstName+controller.lastName;
+        if (controller.fullName == '')
+            controller.fullName = "NA";
+        return controller.fullName;
+    }
+    controller.firstName = '';
+    controller.lastName = '';
+    controller.fullName = '';
+    controller.greet = greet;
+    controller.getFullName = getFullName;
+
+
 }
-angular.module('myapp',[]).controller('MyController', MyController);
+angular.module('myapp', []).controller('MyController', MyController);
