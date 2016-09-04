572b7d697ba0a4427ef52e2b938f27efe8bc8d91
Sathish Jayapal <sjayapal@skminfotech.com>
2016-09-04 07:53:14 -0500

    Saving tasks changes

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,7 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-var TaskController = function () {
-    var controller=this;
-    controller.tasks=['task1','task2','task3'];
+var TaskController = function ($http) {
+    var controller = this;
+    // controller.tasks=['task1','task2','task3'];
+    controller.getTasks = function () {
+        $http.get("/tasks.json").success(function (data) {
+            controller.tasks = data;
+        });
+    }
+    controller.addTask = function () {
+        $http.post("/tasks.json", controller.newTask).success(function (data) {
+            controller.newTask = {};
+            controller.updateMessage = data;
+            controller.getTasks();
+        });
+    }
+    controller.newTask = {};
+    controller.updateMessage='';
+    controller.getTasks();
+    controller.addTask();
+    controller.tasks = [];
 }
-angular.module('todoapp', []).controller('TaskController',TaskController);
+angular.module('todoapp', []).controller('TaskController', TaskController);
