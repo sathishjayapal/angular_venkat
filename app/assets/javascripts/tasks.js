c213c8b7634c736de73ba74cf98876fbd2b5e564
Sathish Jayapal <sjayapal@skminfotech.com>
2016-09-06 05:49:32 -0500

    Sorting not working. But up and plugged.

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,24 +1,29 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-var TaskController = function ($http) {
+var TaskController = function ($http,$filter,$document) {
+
     var controller = this;
-    // controller.tasks=['task1','task2','task3'];
+    controller.newTask = {};
+    controller.updateMessage='';
+    controller.tasks = [];
+
     controller.getTasks = function () {
         $http.get("/tasks.json").success(function (data) {
-            controller.tasks = data;
+            controller.tasks = controller.sortTasks(data);
         });
     }
+    controller.sortTasks=function(tasks){
+        var orderBy= $filter('orderBy');
+        return orderBy(tasks,['taskName','taskDescr']);
+    }
     controller.addTask = function () {
+        alert('Trying post');
         $http.post("/tasks.json", controller.newTask).success(function (data) {
             controller.newTask = {};
             controller.updateMessage = data;
             controller.getTasks();
         });
     }
-    controller.newTask = {};
-    controller.updateMessage='';
-    controller.getTasks();
-    controller.addTask();
-    controller.tasks = [];
+    $document.ready(controller.getTasks);
 }
 angular.module('todoapp', []).controller('TaskController', TaskController);
